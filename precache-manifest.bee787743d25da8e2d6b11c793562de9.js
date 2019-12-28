a000ef152a2ce1f8b0106b87de9342a5c1f89d9f
Bozhidar Zahariev <bozhidar_zahariev@hotmail.com>
2019-12-28 10:46:38 +0000

    Updates

---
+++ precache-manifest.bee787743d25da8e2d6b11c793562de9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d56671e329236f9210a6282f60489d3",
+    "url": "/BojoZahariev/Chameleon/index.html"
+  },
+  {
+    "revision": "749c3d8c03db54fb953e",
+    "url": "/BojoZahariev/Chameleon/static/css/main.0d3c3e27.chunk.css"
+  },
+  {
+    "revision": "3eab951c573f7412f047",
+    "url": "/BojoZahariev/Chameleon/static/js/2.e52c3dbe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/BojoZahariev/Chameleon/static/js/2.e52c3dbe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "749c3d8c03db54fb953e",
+    "url": "/BojoZahariev/Chameleon/static/js/main.0ddc584f.chunk.js"
+  },
+  {
+    "revision": "576c8583ef734e1dba42",
+    "url": "/BojoZahariev/Chameleon/static/js/runtime-main.5f80e132.js"
+  }
+]);
