9883569762d36320ffecd945b938eaecb6858f3c
Bozhidar Zahariev <bozhidar_zahariev@hotmail.com>
2019-12-28 10:58:35 +0000

    Updates

---
+++ precache-manifest.5b71e212736c3ab696c966543b908452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e88ec77261ee0e586374db58e08da9",
+    "url": "/Chameleon/index.html"
+  },
+  {
+    "revision": "749c3d8c03db54fb953e",
+    "url": "/Chameleon/static/css/main.0d3c3e27.chunk.css"
+  },
+  {
+    "revision": "3eab951c573f7412f047",
+    "url": "/Chameleon/static/js/2.e52c3dbe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Chameleon/static/js/2.e52c3dbe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "749c3d8c03db54fb953e",
+    "url": "/Chameleon/static/js/main.0ddc584f.chunk.js"
+  },
+  {
+    "revision": "0b09827c58416ffdff59",
+    "url": "/Chameleon/static/js/runtime-main.353a4b07.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BojoZahariev/Chameleon/precache-manifest.8da529dfeb351edb5376e43aed879a1a.js"
+  "/Chameleon/precache-manifest.5b71e212736c3ab696c966543b908452.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BojoZahariev/Chameleon/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Chameleon/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
